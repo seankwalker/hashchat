13b5cd7dd665fd479944cb82e876f985396086e4
Sean Walker <root@seankwalker.com>
2021-04-16 17:24:01 -0700

    Add Prettier
    ...with accompanying config file and `format` npm script

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: "es5",
+};
